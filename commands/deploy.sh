b55c8cd7f1031a4268c02ad7ea74cdb8729c42ee
Lebid_Dmytro <lebedd99@gmail.com>
2025-12-10 17:10:31 +0200

    Add deploy.yml workflow and update deploy.sh for main branch

--- commands/deploy.sh
+++ commands/deploy.sh
@@ -10,22 +10,22 @@ handle_error() {
 }
 
 # Navigate to the application directory
-cd /home/ubuntu/src/py-fastapi-homework-4-task || handle_error "Failed to navigate to the application directory."
+cd /home/ec2-user/src/py-fastapi-homework-4-task || handle_error "Failed to navigate to the application directory."
 
 # Fetch the latest changes from the remote repository
 echo "Fetching the latest changes from the remote repository..."
-git fetch origin develop || handle_error "Failed to fetch updates from the 'origin' remote."
+git fetch origin main || handle_error "Failed to fetch updates from the 'origin' remote."
 
-# Reset the local repository to match the remote 'develop' branch
-echo "Resetting the local repository to match 'origin/develop'..."
-git reset --hard origin/develop || handle_error "Failed to reset the local repository to 'origin/develop'."
+# Reset the local repository to match the remote 'main' branch
+echo "Resetting the local repository to match 'origin/main'..."
+git reset --hard origin/main || handle_error "Failed to reset the local repository to 'origin/main'."
 
 # (Optional) Pull any new tags from the remote repository
 echo "Fetching tags from the remote repository..."
 git fetch origin --tags || handle_error "Failed to fetch tags from the 'origin' remote."
 
 # Build and run Docker containers with Docker Compose v2
-docker compose -f docker-compose-prod.yml up -d --build || handle_error "Failed to build and run Docker containers using docker-compose-prod.yml."
+sudo docker-compose -f docker-compose-prod.yml up -d --build || handle_error "Failed to build and run Docker containers using docker-compose-prod.yml."
 
 # Print a success message upon successful deployment
 echo "Deployment completed successfully."
